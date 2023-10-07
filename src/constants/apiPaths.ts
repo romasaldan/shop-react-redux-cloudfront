fdc59a37572e9253c7d75f4fea8ca876f4f8c172
Roman Saldan <roman.saldan@photobox.com>
2023-10-07 18:49:59 +0300

    changed endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://615fa4cunf.execute-api.eu-north-1.amazonaws.com/dev",
+  order: "https://615fa4cunf.execute-api.eu-north-1.amazonaws.com/dev",
+  import: "https://615fa4cunf.execute-api.eu-north-1.amazonaws.com/dev",
+  bff: "https://615fa4cunf.execute-api.eu-north-1.amazonaws.com/dev",
+  cart: "https://615fa4cunf.execute-api.eu-north-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
